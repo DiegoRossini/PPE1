9e1f40234160db838636b25fdf2583fdb8bb5fa9
DiegoRossini <49552187+DiegoRossini@users.noreply.github.com>
2022-10-15 16:08:00 +0200

    Delete exercise1_script3.sh

--- Exercises/exercise1_script3.sh
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/bash
-
-bash /home/diego/Desktop/Master\ TAL/Corsi/M1\ S1\ Programmation\ et\ projet\ encadré\ 1/Lezione\ 4/esercizio1_script2.sh 2016 Location
-
-bash /home/diego/Desktop/Master\ TAL/Corsi/M1\ S1\ Programmation\ et\ projet\ encadré\ 1/Lezione\ 4/esercizio1_script2.sh 2017 Location
-
-bash /home/diego/Desktop/Master\ TAL/Corsi/M1\ S1\ Programmation\ et\ projet\ encadré\ 1/Lezione\ 4/esercizio1_script2.sh 2018 Location
